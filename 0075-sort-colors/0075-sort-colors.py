7489f8c5c6aac3b34b2c51670be59f9f85aff8ad
nishant42 <65972909+nishant42@users.noreply.github.com>
2024-12-27 08:25:07 +0530

    Time: 0 ms (100.00%), Space: 17.8 MB (7.40%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -5,17 +5,22 @@ class Solution:
         n-1 = 2
         [0,0,1,1,0,2,1,2]
         """
-        low= 0
+        loww= 0
         mid= 0
         high=len(nums)-1
         while mid<=high:
             if nums[mid] == 0 :
-                nums[mid],nums[low]=nums[low],nums[mid]
-                low+=1
+                #swap mid with low as low is the first element after 0
+                # increment low and mid as new low , mid
+                nums[mid],nums[loww]=nums[loww],nums[mid]
+                loww+=1
                 mid+=1
             elif nums[mid]==1:
+                # mid is just after mid-1 which is also 1 so we just need to 
+                #increment mid 
                 mid+=1
             else:
+                #swap mid with high to get shorted 2 and then decrement high -1
                 nums[mid],nums[high]=nums[high],nums[mid]
                 high-=1
                 
