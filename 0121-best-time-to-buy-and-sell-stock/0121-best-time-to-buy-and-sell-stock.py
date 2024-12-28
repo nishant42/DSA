6645be544e67c8cbcb965605c41007cc2dff96e6
nishant42 <65972909+nishant42@users.noreply.github.com>
2024-12-29 02:21:41 +0530

    Time: 149 ms (8.99%), Space: 26.8 MB (17.46%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,5 +1,13 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        ''' any profit is made by selling the stock at the price at that day 
+        and buying at minimum price before that day , if we had chosen first
+        then sell and buy would be same which results into 0 , thus we started from 
+        second element in the array.
+        min buy price updates by chosing min between the previous min price and current
+        index after finding the cost.
+        return profit if no buy or sell happended then buy default 0 is returned.
+        '''
         profit=0
         buymin=prices[0]
         for i in range(1,len(prices)):
