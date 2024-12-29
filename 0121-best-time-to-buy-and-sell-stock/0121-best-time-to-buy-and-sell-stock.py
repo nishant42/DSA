762643c79ed447f8d19e2bba7d41ec44b133ea8b
nishant42 <65972909+nishant42@users.noreply.github.com>
2024-12-29 15:02:15 +0530

    Time: 147 ms (10.37%), Space: 27.1 MB (7.17%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -7,6 +7,8 @@ class Solution:
         min buy price updates by chosing min between the previous min price and current
         index after finding the cost.
         return profit if no buy or sell happended then buy default 0 is returned.
+        For brute force [0][0] becomes [0][3] and [0][1] become [1][3] -> conclude
+        left side j become right side i and right side j= (n-1)-i for n=4 i=0 ->3   
         '''
         profit=0
         buymin=prices[0]
