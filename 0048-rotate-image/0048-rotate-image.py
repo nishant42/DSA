ce6518b009190a7f4a0fc2d66d4207b1f10c977d
nishant42 <65972909+nishant42@users.noreply.github.com>
2024-12-29 15:04:56 +0530

    Time: 0 ms (100%), Space: 17.9 MB (8.45%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -2,7 +2,11 @@ class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
-        Approach : First transpose the matrix meaning making columns the row 
+        For brute force [0][0] becomes [0][3] and [0][1] become [1][3] -> conclude
+        left side j become right side i and right side j= (n-1)-i for n=4 i=0 ->3   
+        thus you get [0][0]->[0][3]
+
+        Approach (Oprimize): First transpose the matrix meaning making columns the row 
         of the matrix ex.row 0-> 1 2 3 become column 0 -> 1 2 3 . if this goes
         on the you will eventually find row 0 -> 1 4 7 and if you then reverse
         it you will find row 0 -> 7 4 1 . like wise you will find the resultant 
