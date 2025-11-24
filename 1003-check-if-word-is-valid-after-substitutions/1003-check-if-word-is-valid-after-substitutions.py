2131062dad69e991cb2b679a1e690a0e79b0eca4
nishant42 <65972909+nishant42@users.noreply.github.com>
2025-11-24 18:08:18 +0000

    Time: 7 ms (89.57%), Space: 18.2 MB (12.98%) - LeetHub

---
+++ 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        res=[]
+        for i in s:
+            if i=='c':
+                if len(res)>=2 and res[-1]=='b' and res[-2]=='a':
+                    res.pop()
+                    res.pop()
+                else:
+                    return False
+
+            else:
+                res.append(i)
+        if not res:
+            return True
+        else:
+            return False
+        
