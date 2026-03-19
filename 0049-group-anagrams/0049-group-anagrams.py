c338a8df0b1fb2c6a6d0cd97fa5268127c1f7510
nishant42 <65972909+nishant42@users.noreply.github.com>
2026-03-19 15:22:43 +0000

    Time: 15 ms (45.63%), Space: 23.9 MB (32.95%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,25 +1,10 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        mydict={}
-        for ele in strs:
-            if "".join(sorted(ele)) in mydict:
-                mydict["".join(sorted(ele))].append(ele)
-            else:
-                mydict["".join(sorted(ele))]=[ele]
-        return list(mydict.values())        
+        dict1=defaultdict(list)
         
-        
-        
-        
-        
-        
-        
-        
-        """
-        1.create a dictionary
-        2.create a loop and check if the sorted str in that dict if not then add
-        3.print the values of the dictionary
-        
-        
-        """
-        
+        for stri in strs:
+            count= [0]*26
+            for s in stri:
+                count[ord(s)-ord('a')]+=1
+            dict1[tuple(count)].append(stri)
+        return list(dict1.values())
